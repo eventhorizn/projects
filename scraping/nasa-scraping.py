b2cfd76d3605af2a6400896373e56c41304b845c
Gary Hake <Gary.Hake@waystar.com>
2020-08-27 15:53:45 -0400

    nasa scraping done

---
+++ scraping/nasa-scraping.py
@@ -0,0 +1,26 @@
+from splinter import Browser
+
+executable_path = {
+    'executable_path': 'C:\\Users\\gary.hake\\Desktop\\chromedriver_win32\\chromedriver.exe'}
+    
+hemisphere_info = []
+
+with Browser(driver_name="chrome", **executable_path) as browser:
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+    
+    link_containers = browser.links.find_by_partial_text("Hemisphere")
+    links = []
+
+    # This gets me the link object, but I want the url of each
+    for lc in browser.links.find_by_partial_text("Hemisphere"):
+        links.append(lc['href']) # Grabbing the href gets me the url
+
+    #  Cycling through those links to get the title and pic url
+    for link in links:
+        browser.visit(link)
+        title = browser.find_by_tag('h2').text.replace(" Enhanced", "")
+        pic_url = browser.links.find_by_partial_text('Original')['href']
+        hemisphere_info.append({"title" : title, "img_url": pic_url})
+
+print(hemisphere_info)
