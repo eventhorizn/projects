3b74b314afb1f04aa1885f0efd534d502f44ee96
Gary Hake <devgaryhake@gmail.com>
2022-12-04 16:38:38 -0500

    day 3 in the books

--- adventOfCode/2022/day_3/Program.cs
+++ adventOfCode/2022/day_3/Program.cs
@@ -1,12 +1,60 @@
 ﻿var lines = File.ReadAllLines("input.txt");
-var alphPrio = GetAlphabetPriority();
 
-// Part 1
-foreach (var line in lines)
+PartOne(lines);
+PartTwo(lines);
+
+static void PartOne(IList<string> lines)
+{
+    var alphPrio = GetAlphabetPriority();
+    var prioritySum = 0;
+    foreach (var line in lines)
+    {
+        var common = GetCommonCharacter(line);
+        var priority = alphPrio[common];
+        prioritySum += priority;
+    }
+
+    Console.WriteLine(prioritySum);
+}
+
+static char GetCommonCharacter(string line)
 {
     var firstComp = line.Substring(0, line.Length / 2);
-    var last = line.Substring((int)(line.Length / 2), (int)(line.Length / 2));
-    var firstComp =
+    var secondComp = line.Substring(line.Length / 2, line.Length / 2);
+
+    var firstCompSet = new HashSet<char>(firstComp);
+    var secondCompSet = new HashSet<char>(secondComp);
+
+    var intersection = firstCompSet.Intersect(secondCompSet);
+
+    return intersection.ElementAt(0);
+}
+
+static void PartTwo(IList<string> lines)
+{
+    var alphPrio = GetAlphabetPriority();
+    var chunkedLines = lines.Chunk(3);
+    var prioritySum = 0;
+
+    foreach (var chunk in chunkedLines)
+    {
+        var common = GetCommonBadge(chunk);
+        var priority = alphPrio[common];
+        prioritySum += priority;
+    }
+
+    Console.WriteLine(prioritySum);
+}
+
+static char GetCommonBadge(string[] line)
+{
+    var first = new HashSet<char>(line[0]);
+    var second = new HashSet<char>(line[1]);
+    var third = new HashSet<char>(line[2]);
+
+    var intersection = first.Intersect(second.Intersect(third));
+
+    return intersection.ElementAt(0);
 }
 
 static IDictionary<char, int> GetAlphabetPriority()
