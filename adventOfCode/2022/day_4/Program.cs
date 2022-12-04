d09fa4e46594fed6fe89d5749fedb8a80ed3fb96
Gary Hake <devgaryhake@gmail.com>
2022-12-04 18:09:19 -0500

    start of day 4

---
+++ adventOfCode/2022/day_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+static void PartOne(IList<string> lines)
+{
+
+}
+
+static void PartTwo(IList<string> lines)
+{
+
+}
