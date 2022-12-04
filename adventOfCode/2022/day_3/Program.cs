9c7f68fb9b48c86f6a8fd40adf9b55b82e9ca7b3
Gary Hake <devgaryhake@gmail.com>
2022-12-04 16:14:41 -0500

    checking this in laptop taking forever

---
+++ adventOfCode/2022/day_3/Program.cs
@@ -0,0 +1,23 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var alphPrio = GetAlphabetPriority();
+
+// Part 1
+foreach (var line in lines)
+{
+    var firstComp = line.Substring(0, line.Length / 2);
+    var last = line.Substring((int)(line.Length / 2), (int)(line.Length / 2));
+    var firstComp =
+}
+
+static IDictionary<char, int> GetAlphabetPriority()
+{
+    const string alph = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var alphPrio = new Dictionary<char, int>();
+
+    for (var i = 0; i < alph.Length; i++)
+    {
+        alphPrio.Add(alph[i], i + 1);
+    }
+
+    return alphPrio;
+}
