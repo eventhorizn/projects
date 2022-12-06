0d9d91b7d392f4cb7272d292da39a92a00623f10
Gary Hake <devgaryhake@gmail.com>
2022-12-05 21:54:45 -0500

    current progress

--- adventOfCode/2022/day_5/Program.cs
+++ adventOfCode/2022/day_5/Program.cs
@@ -35,27 +35,28 @@ static void PartOne(IList<string> lines)
 
 static void PartTwo(IList<string> lines)
 {
-    var stacks = BuildQueues();
+    var stacks = BuildLinkedLists();
     for (var i = 0; i < lines.Count; i++)
     {
         // skip the stacks
-        if (i < 10) continue;
+        if (i < 5) continue;
 
         var instr = new Instruction(lines[i]);
-        var fromStack = stacks[instr.From - 1];
-        var toStack = stacks[instr.To - 1];
+        var from = stacks[instr.From - 1];
+        var to = stacks[instr.To - 1];
 
         for (var j = 0; j < instr.Amount; j++)
         {
-            var val = fromStack.Dequeue();
-            toStack.Enqueue(val);
+            var val = from.Last.Value;
+            from.RemoveLast();
+            to.AddLast(val);
         }
     }
 
     var output = "";
     foreach (var stack in stacks)
     {
-        output += stack.Dequeue();
+        output += stack.Last.Value;
     }
 
     Console.WriteLine(output);
@@ -114,37 +115,44 @@ static IList<Stack<char>> BuildStacks()
 // [P] [C] [N] [T] [S] [F] [R] [G] [Q]
 // [Z] [P] [S] [F] [F] [T] [N] [P] [W]
 //  1   2   3   4   5   6   7   8   9 
-static IList<Queue<char>> BuildQueues()
+static IList<LinkedList<char>> BuildLinkedLists()
 {
-    var queues = new List<Queue<char>>();
-    var queueOne = new Queue<char>(
-        new List<char>() { 'Z', 'P', 'M', 'H', 'R' });
-    var queueTwo = new Queue<char>(
-        new List<char>() { 'P', 'C', 'J', 'B' });
-    var queueThree = new Queue<char>(
-        new List<char>() { 'S', 'N', 'H', 'G', 'L', 'C', 'D' });
-    var queueFour = new Queue<char>(
-        new List<char>() { 'F', 'T', 'M', 'D', 'Q', 'S', 'R', 'L' });
-    var queueFive = new Queue<char>(
-        new List<char>() { 'F', 'S', 'P', 'Q', 'B', 'T', 'Z', 'M' });
-    var queueSix = new Queue<char>(
-        new List<char>() { 'T', 'F', 'S', 'Z', 'B', 'G' });
-    var queueSeven = new Queue<char>(
-        new List<char>() { 'N', 'R', 'V' });
-    var queueEight = new Queue<char>(
-        new List<char>() { 'P', 'G', 'L', 'T', 'D', 'V', 'C', 'M' });
-    var queueNine = new Queue<char>(
-        new List<char>() { 'W', 'Q', 'N', 'J', 'F', 'M', 'L' });
+    var queues = new List<LinkedList<char>>();
+    var queueOne = new LinkedList<char>(
+        new List<char>() { 'Z', 'N' });
+    var queueTwo = new LinkedList<char>(
+        new List<char>() { 'M', 'C', 'D' });
+    var queueThree = new LinkedList<char>(
+        new List<char>() { 'P' });
+
+    // var queueOne = new LinkedList<char>(
+    //     new List<char>() { 'Z', 'P', 'M', 'H', 'R' });
+    // var queueTwo = new LinkedList<char>(
+    //     new List<char>() { 'P', 'C', 'J', 'B' });
+    // var queueThree = new LinkedList<char>(
+    //     new List<char>() { 'S', 'N', 'H', 'G', 'L', 'C', 'D' });
+    // var queueFour = new LinkedList<char>(
+    //     new List<char>() { 'F', 'T', 'M', 'D', 'Q', 'S', 'R', 'L' });
+    // var queueFive = new LinkedList<char>(
+    //     new List<char>() { 'F', 'S', 'P', 'Q', 'B', 'T', 'Z', 'M' });
+    // var queueSix = new LinkedList<char>(
+    //     new List<char>() { 'T', 'F', 'S', 'Z', 'B', 'G' });
+    // var queueSeven = new LinkedList<char>(
+    //     new List<char>() { 'N', 'R', 'V' });
+    // var queueEight = new LinkedList<char>(
+    //     new List<char>() { 'P', 'G', 'L', 'T', 'D', 'V', 'C', 'M' });
+    // var queueNine = new LinkedList<char>(
+    //     new List<char>() { 'W', 'Q', 'N', 'J', 'F', 'M', 'L' });
 
     queues.Add(queueOne);
     queues.Add(queueTwo);
     queues.Add(queueThree);
-    queues.Add(queueFour);
-    queues.Add(queueFive);
-    queues.Add(queueSix);
-    queues.Add(queueSeven);
-    queues.Add(queueEight);
-    queues.Add(queueNine);
+    // queues.Add(queueFour);
+    // queues.Add(queueFive);
+    // queues.Add(queueSix);
+    // queues.Add(queueSeven);
+    // queues.Add(queueEight);
+    // queues.Add(queueNine);
 
     return queues;
 }
