51c48acab011ffcc77b76ef813211584d15bcff6
Gary Hake <devgaryhake@gmail.com>
2022-12-06 16:43:53 -0500

    day 6 in the books

--- adventOfCode/2022/day_6/Program.cs
+++ adventOfCode/2022/day_6/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt");
+
+PartOne(lines);
+PartTwo(lines);
+
+static void PartOne(IList<string> lines)
+{
+    // Just one line
+    var answer = GetStartOfPacket(lines[0], 4);
+    Console.WriteLine(answer);
+}
+
+static void PartTwo(IList<string> lines)
+{
+    // Just one line
+    var answer = GetStartOfPacket(lines[0], 14);
+    Console.WriteLine(answer);
+}
+
+static int GetStartOfPacket(string line, int markerLength)
+{
+    var lineList = new LinkedList<char>(line);
+    var workingList = new LinkedList<char>();
+    var loopList = new LinkedList<char>(line);
+    // seed list
+    for (var i = 0; i < loopList.Count; i++)
+    {
+        workingList.AddLast(lineList.First.Value);
+        lineList.RemoveFirst();
+
+        if (workingList.Count == markerLength && IsUnique(workingList))
+            return i + 1;
+        else if (workingList.Count == markerLength)
+            workingList.RemoveFirst();
+    }
+
+    return 0;
+}
+
+static bool IsUnique(LinkedList<char> list)
+{
+    return list.Distinct().Count() == list.Count();
+}
