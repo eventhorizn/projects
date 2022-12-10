831b83c2fc46bc22d567ca43b526bdec258df5e4
Gary Hake <devgaryhake@gmail.com>
2022-12-10 10:34:20 -0500

    part 2 done

--- adventOfCode/2022/day_10/Program.cs
+++ adventOfCode/2022/day_10/Program.cs
@@ -2,7 +2,7 @@
 
 var lines = File.ReadAllLines("input.txt");
 
-PartOne(lines);
+//PartOne(lines);
 PartTwo(lines);
 
 static void PartOne(IList<string> lines)
@@ -44,4 +44,83 @@ static void PartOne(IList<string> lines)
 
 static void PartTwo(IList<string> lines)
 {
+    var register = 1;
+    var stringOutput = new List<string>();
+    var crtPos = 0;
+    var output = "";
+    for (var i = 0; i < lines.Count; i++)
+    {
+        var lineSplit = lines[i].Split(" ");
+
+        if (lineSplit[0] == "noop")
+        {
+            if (crtPos == register ||
+                crtPos == register + 1 ||
+                crtPos == register - 1)
+            {
+                output += "#";
+            }
+            else
+            {
+                output += ".";
+            }
+
+            crtPos++;
+
+            if (output.Length == 40)
+            {
+                stringOutput.Add(output);
+                output = "";
+                crtPos = 0;
+            }
+        }
+        if (lineSplit[0] == "addx")
+        {
+            if (crtPos == register ||
+                crtPos == register + 1 ||
+                crtPos == register - 1)
+            {
+                output += "#";
+            }
+            else
+            {
+                output += ".";
+            }
+
+            crtPos++;
+
+            if (output.Length == 40)
+            {
+                stringOutput.Add(output);
+                output = "";
+                crtPos = 0;
+            }
+
+            if (crtPos == register ||
+                crtPos == register + 1 ||
+                crtPos == register - 1)
+            {
+                output += "#";
+            }
+            else
+            {
+                output += ".";
+            }
+
+            register += int.Parse(lineSplit[1]);
+            crtPos++;
+
+            if (output.Length == 40)
+            {
+                stringOutput.Add(output);
+                output = "";
+                crtPos = 0;
+            }
+        }
+    }
+
+    foreach (var lineOut in stringOutput)
+    {
+        Console.WriteLine(lineOut);
+    }
 }
