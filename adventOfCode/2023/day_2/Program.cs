6647098b42c6558535787bbbb3f63f65c2ef2630
Gary Hake <devgaryhake@gmail.com>
2023-12-03 21:40:58 -0500

    part 2 done

--- adventOfCode/2023/day_2/Program.cs
+++ adventOfCode/2023/day_2/Program.cs
@@ -10,6 +10,7 @@ var baseCubes = new Dictionary<string, int>
     { blue, 14 },
 };
 
+// Part 1
 var total = 0;
 
 foreach (var line in lines)
@@ -64,4 +65,59 @@ bool GameValid(string[] randomCubes)
 }
 
 Console.WriteLine("Part 1");
+Console.WriteLine(total);
+
+// Part 2
+total = 0;
+
+foreach (var line in lines)
+{
+    // Get game number
+    var gameNumber = line
+        .Substring(0, line.IndexOf(":"))
+        .Replace("Game ", "");
+    var gameLength = line.IndexOf(":") + 2;
+    var randomCubes = line
+        .Substring(gameLength, line.Length - gameLength)
+        .Split(";");
+
+    var allCubes = new List<string>();
+    foreach (var rc in randomCubes)
+    {
+        var cubeList = rc.Split(',');
+        foreach (var cube in cubeList)
+        {
+            allCubes.Add(cube.Replace(" ", ""));
+        }
+    }
+
+    var maxColorVal = new Dictionary<string, int>();
+
+    foreach (var cube in allCubes)
+    {
+        var key = "";
+        if (cube.Contains(red)) key = red;
+        if (cube.Contains(green)) key = green;
+        if (cube.Contains(blue)) key = blue;
+
+        var number = int.Parse(cube.Replace(key, ""));
+
+        if (maxColorVal.ContainsKey(key))
+        {
+            if (maxColorVal[key] < number) maxColorVal[key] = number;
+        }
+        else
+        {
+            maxColorVal[key] = number;
+        }
+    }
+
+    var power = 1;
+    foreach (var val in maxColorVal.Values)
+        power *= val;
+
+    total += power;
+}
+
+Console.WriteLine("Part 2");
 Console.WriteLine(total);
